a62f1ff042f3ed21024ece33868f95ec288bde07
ricardoismael <rirojas1970@gmail.com>
2024-08-09 19:23:11 -0300

    change 3

---
+++ clock/script.js
@@ -0,0 +1,21 @@
+"use strict";
+const secondHand = document.querySelector(".second-hand");
+const minuteHand = document.querySelector(".minute-hand");
+const hourHand = document.querySelector(".hour-hand");
+function setDate() {
+  const now = new Date();
+  const getSecond = now.getSeconds();
+  const secondDegree = (getSecond / 60) * 360 + 90;
+  secondHand.style.transform = `rotate(${secondDegree}deg)`;
+
+  const getMinute = now.getMinutes();
+  const minuteDegree = (getMinute / 60) * 360 + 90;
+  minuteHand.style.transform = `rotate(${minuteDegree}deg)`;
+
+  const getHour = now.getHours();
+  const hourDegree = (getHour / 12) * 360 + 90;
+  hourHand.style.transform = `rotate(${hourDegree}deg)`;
+  console.log(getHour);
+}
+
+setInterval(setDate, 1000);
